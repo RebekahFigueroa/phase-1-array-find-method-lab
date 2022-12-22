a97ba016b88639c2716ba9317a0273d7ee1b5661
Rebekah <figueroarebekah@gmail.com>
2022-12-22 13:11:21 -0600

    commit tree 144cf291fe96712fb001973880f8c8bf21c51053

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+const isWin = function (element) {
+  return element.result === "W";
+};
+
+function superbowlWin(record) {
+  const firstWin = record.find(isWin);
+  //   if (firstWin === undefined) {
+  //     return undefined;
+  //   }
+  //   return firstWin.year;
+
+  return firstWin ? firstWin.year : undefined;
+}
